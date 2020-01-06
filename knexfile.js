66e0c69d466c19caa93553488e36fce91e280d5b
CJ Lucido <c.jose.lucido@gmail.com>
2020-01-06 10:56:33 -0800

    added testing env to knexfile

--- knexfile.js
+++ knexfile.js
@@ -17,4 +17,22 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  testing: {//knex migrate:latest --env testing
+    client: 'sqlite3',
+    connection: {
+      filename: './database/tester.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn,done)=>{//make sure this is in production object too later
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    },
+  },
 };
