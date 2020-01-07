38b4c9e40c827930fe6ec42a2cb23d4235a6ba5b
CJ Lucido <c.jose.lucido@gmail.com>
2020-01-07 09:31:11 -0800

    get all tickets test working, get tickets by helper ID not working because user is undefined by the time I'm running the get request, why

--- 02-tickets/tickets-router.spec.js
+++ 02-tickets/tickets-router.spec.js
@@ -7,6 +7,7 @@ describe('tickets-router.js', () => {
     beforeEach(async () => {
         await db('tickets').truncate();
         await db('students').truncate();
+        await db('helpers').truncate();
     });
 
 
@@ -25,7 +26,7 @@ describe('ticket POST', () => {
 
 
     it('should check to see if a new ticket was created by a student', async () => {
-        const formData = {username: "Sextus", password: "sevint_!#ded"}
+       
                     return testingFrom(server)
                     .post('/api/auth/login')
                     .send({username: "Septimus", password: "sevint_!#ded"})
@@ -53,5 +54,192 @@ describe('ticket POST', () => {
                 });
     }
 )
-});
 
+
+    describe('tickets GET ALL', () => {
+
+        it('should register Septimus if he doesn\'t exist', function(){
+            return testingFrom(server)
+                    .post('/api/auth/register')
+                    .send({username: "Septimus", password: "sevint_!#ded"})
+                    .then(res => {
+                        expect(res.status).toBe(201)
+                    });
+        }
+    ) 
+
+    it('should register Octavia if she doesn\'t exist', function(){
+        return testingFrom(server)
+                .post('/api/auth/register')
+                .send({username: "Octavia", password: "ateth_!#ded"})
+                .then(res => {
+                    expect(res.status).toBe(201)
+                });
+    }
+) 
+    
+    
+        it('should check to see if a new ticket was created by a student', async () => {
+           
+                        return testingFrom(server)
+                        .post('/api/auth/login')
+                        .send({username: "Septimus", password: "sevint_!#ded"})
+                        .then(res => {
+                             let tokenValue = res.body.token;
+                            return testingFrom(server)
+                            .post('/api/students')
+                            .set('Authorization', tokenValue)
+                            .then(res => {
+                                let tokenValue = res.body.token;
+                                let pathId = res.body.user.student_id
+                                return testingFrom(server)
+                                .post(`/api/tickets/create/${pathId}`)
+                                .set('Authorization', tokenValue)
+                                .send({	category: "React",
+                                title: "auth tokens",
+                                description:"how to capture and replace tokens",
+                                what_was_tried:"nothing"})
+                                .then(res => {
+                                    expect(res.status).toBe(201)
+    
+                                })
+                            });
+                        });
+                    });
+
+
+                    it('should check for ALL tickets using helper id, should return empty array due to truncation', async () => {
+           
+                        return testingFrom(server)
+                        .post('/api/auth/login')
+                        .send({username: "Octavia", password: "ateth_!#ded"})
+                        .then(res => {
+                             let tokenValue = res.body.token;
+                            return testingFrom(server)
+                            .post('/api/helpers')
+                            .set('Authorization', tokenValue)
+                            .then(res => {
+                                let tokenValue = res.body.token;
+                                let pathId = res.body.user.helper_id
+                                return testingFrom(server)
+                                .get(`/api/tickets/helper`)
+                                .set('Authorization', tokenValue)
+                                .then(res => {
+                                    expect(Array.isArray(res.body)).toBe(true)
+    
+                                })
+                            });
+                        });
+                    });
+
+
+
+
+
+
+        })
+    
+
+        describe('tickets GET HELPER Tickets', () => {
+
+            it('should register Septimus if he doesn\'t exist', function(){
+                return testingFrom(server)
+                        .post('/api/auth/register')
+                        .send({username: "Septimus", password: "sevint_!#ded"})
+                        .then(res => {
+                            expect(res.status).toBe(201)
+                        });
+            }
+        ) 
+    
+        it('should register Octavia if she doesn\'t exist', function(){
+            return testingFrom(server)
+                    .post('/api/auth/register')
+                    .send({username: "Octavia", password: "ateth_!#ded"})
+                    .then(res => {
+                        expect(res.status).toBe(201)
+                    });
+        }
+    ) 
+        
+        
+            it('should return tickets created by Octavia only', async () => {
+               
+                            return testingFrom(server)
+                            .post('/api/auth/login')
+                            .send({username: "Septimus", password: "sevint_!#ded"})
+                            .then(res => {
+                                 let tokenValue = res.body.token;
+                                return testingFrom(server)
+                                .post('/api/students')
+                                .set('Authorization', tokenValue)
+                                .then(res => {
+                                    let tokenValue = res.body.token;
+                                    let pathId = res.body.user.student_id
+                                    return testingFrom(server)
+                                    .post(`/api/tickets/create/${pathId}`)
+                                    .set('Authorization', tokenValue)
+                                    .send({	category: "React",
+                                    title: "auth tokens",
+                                    description:"how to capture and replace tokens",
+                                    what_was_tried:"nothing"})
+                                    .then(res => {
+                                        return testingFrom(server)
+                                        .post('/api/auth/login')
+                                        .send({username: "Octavia", password: "ateth_!#ded"})
+                                        .then(res => {
+                                             let tokenValue = res.body.token;
+                                            return testingFrom(server)
+                                            .post('/api/helpers')
+                                            .set('Authorization', tokenValue)
+                                            .then(res => {
+                                                let tokenValue = res.body.token;
+                                               
+
+                                                return testingFrom(server)
+                                                        .post('/api/students')
+                                                        .set('Authorization', tokenValue)
+                                                        .then(res => {
+                                                            let tokenValue = res.body.token;
+                                                            let pathId = res.body.user.student_id
+                                                            
+                                                            return testingFrom(server)
+                                                            .post(`/api/tickets/create/${pathId}`)
+                                                            .set('Authorization', tokenValue)
+                                                            .send({	category: "Vue",
+                                                            title: "wow",
+                                                            description:"type",
+                                                            what_was_tried:"nothing"})
+                                                            .then(res=> {
+                                                                //let pathId = res.body.user.helper_id
+                                                                console.log(res.body.user) //UNDEFINED????
+                                                                let studentId = res.body.user.student_id
+                                                                return testingFrom(server)
+                                                                .get(`/api/tickets/helper/${pathId}`)
+                                                                .set('Authorization', tokenValue)
+                                                                .then(res => {
+                                                                    expect(res.body[0].student_id).toMatch(studentId)
+                                    
+                                                                })
+                                                                    
+                                                                    });
+                                        });
+        
+                                    })
+                                });
+                            });
+                        });
+    
+    
+    
+    
+    
+    
+            })
+            })
+        
+        
+        
+        
+        
+        })})
